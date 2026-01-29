868a8f38b1093d63d9e8951fabcc82e915b030c8
ottobeckedorff <otto@beckedorff.com.br>
2026-01-29 15:26:32 -0300

    Update email sender to Verified Domain to enable sending to Gmail

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -13,7 +13,7 @@ export async function POST(request: Request) {
         }
 
         const data = await resend.emails.send({
-            from: 'Pré-Consulta <onboarding@resend.dev>', // Em produção, usar seu domínio verificado
+            from: 'Pré-Consulta Dr. Otto <agendamento@drotto.com.br>', // Domínio verificado
             to: [process.env.DOCTOR_EMAIL || 'benedetjv@gmail.com'], // E-mail para onde o relatório será enviado
             replyTo: patientEmail,
             subject: subject || 'Novo Relatório de Pré-Consulta',
